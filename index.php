394995e06ce16d9e9a24ddbff9171d21c4c3d095
munifahsan <munifahsan.mb9@gmail.com>
2018-01-23 14:00:18 +0700

    munif(23-jan-18)

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 
 <head>
-    <meta charset="UTF-8">
+       <meta charset="UTF-8">
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
     <title>Sign In | Bootstrap Based Admin Template - Material Design</title>
     <!-- Favicon-->
